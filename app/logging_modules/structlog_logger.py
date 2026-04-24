7540198bddd3f7dce386eb7d64c73db0e82b1178
Chimaobi Iwu <ci24aau@herts.ac.uk>
2026-04-24 07:40:59 +0100

    Fixed failure logic in services code

--- app/logging_modules/structlog_logger.py
+++ app/logging_modules/structlog_logger.py
@@ -1,10 +1,22 @@
 import structlog
+import logging
 
-# Configure structlog for JSON structured logs
+logging.basicConfig(
+    format="%(message)s",
+    level=logging.INFO,
+)
+
 structlog.configure(
     processors=[
+        structlog.processors.TimeStamper(fmt="iso"),
+        structlog.processors.add_log_level,
+
+        # reason for error
+        structlog.processors.format_exc_info,
+
         structlog.processors.JSONRenderer()
-    ]
+    ],
+    wrapper_class=structlog.make_filtering_bound_logger(logging.INFO),
 )
 
 logger = structlog.get_logger()
